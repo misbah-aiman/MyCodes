6959f5ce700130639bb17aeb83df244c06333f9c
Misbah Aiman <misbahaiman65@gmail.com>
2024-09-23 16:19:57 +0500

    Add files via upload

---
+++ Increment/post increment/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int misbah = 5;
+    cout << "value of variable is " << misbah << endl;
+    cout << "value before post increment is " << misbah << endl;
+    misbah++;
+    cout << "value after post increment is " << misbah << endl;
+    return 0;
+}
---
+++ Increment/pre increment/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int misbah = 5;
+    cout << "value of variable is " << misbah << endl;
+    cout << "value before pre increment is " << misbah << endl;
+    ++misbah;
+    cout << "value after pre increment is " << misbah << endl;
+    return 0;
+}
