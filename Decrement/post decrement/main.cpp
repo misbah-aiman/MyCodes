4aa03b4f6add72c1bcff53df9961d28aa2988700
Misbah Aiman <misbahaiman65@gmail.com>
2024-09-26 13:46:38 +0500

    Add files via upload

---
+++ Decrement/post decrement/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int misbah = 5;
+    cout << "value of variable is " << misbah << endl;
+    cout << "value before post decrement is " << misbah << endl;
+    misbah--;
+    cout << "value after post decrement is " << misbah << endl;
+    return 0;
+}
