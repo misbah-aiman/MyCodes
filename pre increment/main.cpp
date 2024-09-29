ac053303bab5c35bec2188ca58e048394fafe98b
Misbah Aiman <misbahaiman65@gmail.com>
2024-09-29 14:23:25 +0500

    Add files via upload

---
+++ pre increment/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int misbah = 5;//declaration and initialization
+    cout << "value of variable is " << misbah << endl;//initial value of variable
+    cout << "value before pre-increment is " << misbah << endl;
+    ++misbah;
+    cout << "value after pre-increment is " << misbah << endl;
+    return 0;
+}
